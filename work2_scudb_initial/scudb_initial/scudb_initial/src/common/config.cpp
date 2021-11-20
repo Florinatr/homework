4165b53e18e9fba09cd59188f26e6bc23381f58a
tangrun <562786553@qq.com>
2021-11-20 21:10:55 +0800

    再次更改git

--- work2_scudb_initial/scudb_initial/scudb_initial/src/common/config.cpp
+++
@@ -1,7 +0,0 @@
-#include "common/config.h"
-
-namespace scudb {
-  std::atomic<bool> ENABLE_LOGGING(false);  // for virtual table
-  std::chrono::duration<long long int> LOG_TIMEOUT =
-   std::chrono::seconds(1);
-}
